c7b79e2de7a78128e05768d8571ee6124cf4bfea
José <josepaulo1607@gmail.com>
2025-07-08 09:07:23 -0300

    feat: add validation to POST /candidate request

--- src/main/java/br/com/josepaulo/gestao_vagas/modules/candidate/CandidateController.java
+++ src/main/java/br/com/josepaulo/gestao_vagas/modules/candidate/CandidateController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
