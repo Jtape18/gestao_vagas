6c8c3b368dd29578bea698b377e5bf25b127dbea
José <josepaulo1607@gmail.com>
2025-07-10 10:52:50 -0300

    refactor: improve structure and readability of JobController

--- src/main/java/br/com/josepaulo/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/josepaulo/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -1,19 +1,17 @@
 package br.com.josepaulo.gestao_vagas.modules.company.controllers;
 
+import javax.security.sasl.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.josepaulo.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.josepaulo.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
-
-import javax.security.sasl.AuthenticationException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/auth")
