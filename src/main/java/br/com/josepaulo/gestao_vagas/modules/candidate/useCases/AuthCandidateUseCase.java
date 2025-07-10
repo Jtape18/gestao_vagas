ad784b1ea1d8807ff0e08d204e5117041df317d7
José <josepaulo1607@gmail.com>
2025-07-10 13:01:37 -0300

    feat: create AuthCandidateController for login endpoint

--- src/main/java/br/com/josepaulo/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/br/com/josepaulo/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -22,40 +22,44 @@ import br.com.josepaulo.gestao_vagas.modules.candidate.repositories.CandidateRep
 @Service
 public class AuthCandidateUseCase {
 
-    @Value("${security.token.secret.candidate}")
-    private String secretKey;
+        @Value("${security.token.secret.candidate}")
+        private String secretKey;
 
-    @Autowired
-    private CandidateRepository candidateRepository;
+        @Autowired
+        private CandidateRepository candidateRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+        @Autowired
+        private PasswordEncoder passwordEncoder;
 
-    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
-            throws AuthenticationException {
-        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
-                .orElseThrow(() -> {
-                    throw new UsernameNotFoundException("Username/Password incorrect");
-                });
-        var passwordMatches = this.passwordEncoder
-                .matches(authCandidateRequestDTO.password(), candidate.getPassword());
-        if (!passwordMatches) {
-            throw new AuthenticationException();
+        public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+                        throws AuthenticationException {
+                var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+                                .orElseThrow(() -> {
+                                        throw new UsernameNotFoundException("Username/Password incorrect");
+                                });
+                var passwordMatches = this.passwordEncoder
+                                .matches(authCandidateRequestDTO.password(), candidate.getPassword());
+                if (!passwordMatches) {
+                        throw new AuthenticationException();
+                }
+
+                Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+                var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
+
+                var token = JWT.create()
+                                .withIssuer("javagas")
+                                .withSubject(candidate.getId().toString())
+                                .withClaim("roles", Arrays.asList("candidate"))
+                                .withExpiresAt(expiresIn)
+                                .sign(algorithm);
+
+                var authCandidateResponse = AuthCandidateResponseDTO.builder()
+                                .access_token(token)
+                                .expires_in(expiresIn.toEpochMilli())
+                                .build();
+
+                return authCandidateResponse;
         }
-
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        var token = JWT.create()
-                .withIssuer("javagas")
-                .withSubject(candidate.getId().toString())
-                .withClaim("roles", Arrays.asList("candidate"))
-                .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
-                .sign(algorithm);
-
-        var authCandidateResponse = AuthCandidateResponseDTO.builder()
-                .access_token(token)
-                .build();
-
-        return authCandidateResponse;
-    }
 
 }
