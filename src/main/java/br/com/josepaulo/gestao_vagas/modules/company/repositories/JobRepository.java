8cb26158ce0925b2fc348d67efd2130854d1d246
José <josepaulo1607@gmail.com>
2025-07-14 12:12:14 -0300

    feat: add authentication to Swagger

--- src/main/java/br/com/josepaulo/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/josepaulo/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.josepaulo.gestao_vagas.modules.company.entities.JobEntity;
 
+@Repository
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
 
-    List<JobEntity> findByDescriptionContaining(String filter);
+    List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
 
 }
